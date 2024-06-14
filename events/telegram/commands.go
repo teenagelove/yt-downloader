4dd3ae5b591a26d7973bd8f5193f5970be7b81c1
teenagelove <dani4upa586@gmail.com>
2024-06-14 19:33:19 +0900

    fix nixpacks.toml try n20

--- events/telegram/commands.go
+++ events/telegram/commands.go
@@ -35,7 +35,7 @@ func (p *Processor) doCmd(ctx context.Context, text string, chatID int, username
 	// Проверяем значение PATH после добавления нового пути.
 	fmt.Println("Текущее значение PATH:", os.Getenv("PATH"))
 
-	output, err := converter.ExecuteCommand("ls /bin")
+	output, err := converter.ExecuteCommand("ls /usr/bin/")
 	if err != nil {
 		fmt.Println("Ошибка:", err)
 	} else {
