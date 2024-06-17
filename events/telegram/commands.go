d14962bee623958617509e27da04e9e1086a7361
teenagelove <dani4upa586@gmail.com>
2024-06-17 12:48:34 +0900

    fix nixpacks.toml try n47

--- events/telegram/commands.go
+++ events/telegram/commands.go
@@ -34,7 +34,7 @@ func (p *Processor) doCmd(ctx context.Context, text string, chatID int, username
 	// Проверяем значение PATH после добавления нового пути.
 	//fmt.Println("Текущее значение PATH:", os.Getenv("PATH"))
 
-	ls, err := converter.ExecuteCommand("ls /root/.nix-profile/bin/")
+	ls, err := converter.ExecuteCommand("find / -name ffmpeg 2>/dev/null")
 	if err != nil {
 		fmt.Println("Ошибка:", err)
 	} else {
